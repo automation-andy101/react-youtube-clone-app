a3c4fd6bf4afe8000f22cd354a16bfa76d63c004
Andy Short <automation.andy101@gmail.com>
2023-04-14 09:04:00 +0100

    Added sidebar, searechbar and navigation components

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import SideBar from './SideBar';
 
 
 const Feed = () => {
+  const [] = useState();
+  
+
   return (
-    <div>
-      
-    </div>
+
+    // Normally flex direction is set to 'column', on medium and larger devices flex direction is set to 'row'
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row'}}}>
+
+      {/* Normally height is set to 'auto', on medium and larger devices height is set to a view height of '92vh' */}
+      <Box sx={{ 
+        height: { sx: 'auto', md: '92vh' },  
+        borderRight: '1px solid #3d3d3d',
+        px: { sx: 0, md: 2 }  
+      }}>
+        <SideBar />
+
+        <Typography 
+          className='copyright' 
+          variant='body2' 
+          sx={{ 
+            mt: 1.5,
+            color: '#fff'
+          }}
+        >
+          Copyright 2023 Andy Short
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Stack } from '@mui/material';
+
+import { categories } from '../utils/constants';
+
+
+const SideBar = () => {
+    const selectedCategory = 'New';
+
+    return (
+        <Stack 
+            direction='row'
+            sx={{
+                overflowY: 'auto',
+                height: { sx: 'auto', md: '95%' },
+                flexDirection: { sx: 'row', md: 'column' }
+            }}
+        >
+        {
+            categories.map((category, index) => (
+                <button 
+                    className='category-btn'
+                    style={{ 
+                        background: category.name === selectedCategory && '#FC1503',
+                        color: 'white'
+                    }}
+                    key={category.name}
+                >
+                    <span>{category.icon}</span>
+                    <span>{category.name}</span>
+                </button>
+            ))
+        }
+        </Stack>
+  )
+}
+
+export default SideBar;
