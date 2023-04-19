bf74a4bb7820b4d8d2adf922a9eff3654df80c37
Andy Short <automation.andy101@gmail.com>
2023-04-19 19:40:45 +0100

    Finished project

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box, CircularProgress, Stack } from '@mui/material';
 
-
 const Loader = () =>  (
   <Box minHeight="95vh">
     <Stack direction='row' justifyContent='center' alignItems='center' height='80vh' >
